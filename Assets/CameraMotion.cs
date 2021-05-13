e7a9476a27b08161cd6154282971fb10b6962673
Tomohiko Mukai <contact@mukai-lab.org>
2021-05-13 11:05:45 +0900

    GP1-04基礎課題の解法

--- Assets/CameraMotion.cs
+++ Assets/CameraMotion.cs
@@ -11,6 +11,35 @@ public class CameraMotion : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * 0.1f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * 0.1f);
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            Vector3 angle = transform.eulerAngles;
+            angle.y += 1.0f;
+            transform.eulerAngles = angle;
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            Vector3 angle = transform.eulerAngles;
+            angle.x -= 1.0f;
+            transform.eulerAngles = angle;
+        }
+        if (Input.GetKey(KeyCode.T))
+        {
+            Camera camera = GetComponent<Camera>();
+            camera.fieldOfView -= 1.0f;
+        }
+        if (Input.GetKey(KeyCode.G))
+        {
+            Camera camera = GetComponent<Camera>();
+            camera.fieldOfView += 1.0f;
+        }
     }
 }
---
+++ Assets/CameraMotion3.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotion3 : MonoBehaviour
+{
+    Vector3 posA;
+    Vector3 posB;
+    Vector3 angleA;
+    Vector3 angleB;
+    //float fovA;
+    //float fovB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posA.Set(0, 0, -5); // カメラ地点A
+        posB.Set(0, 0, -3); // カメラ地点B
+        angleA.Set(0, 0, 0);
+        angleB.Set(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            // カメラを視点Aに
+            transform.position = posA;
+            transform.eulerAngles = angleA;
+            // Camera.main.fieldOfView = fovA;
+        }
+        else if (Input.GetKey(KeyCode.B))
+        {
+            // カメラを視点Bに
+            transform.position = posB;
+            transform.eulerAngles = angleB;
+            // Camera.main.fieldOfView = fovB;
+        }
+    }
+}
